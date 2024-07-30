7b058dfd05c3fbd41fc417c84d426c5f0e59ddbb
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2024-07-30 23:21:27 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// connect cloudAmqp
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://hqnqjkbe:w9vUNnpPxRcF3o8N11VXEuz43qVpXx-y@sparrow.rmq.cloudamqp.com/hqnqjkbe");
+
+// create connection
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// create queue
+channel.QueueDeclare(queue: "example-queue", exclusive: false); // exclusive = many queue can subscribe
+
+// RabbitMq accepts queues of type byte
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", basicProperties: null, message); // exchange: "" default exchange(direct)
+
+Console.ReadLine();
---
+++ RabbitMq.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://hqnqjkbe:w9vUNnpPxRcF3o8N11VXEuz43qVpXx-y@sparrow.rmq.cloudamqp.com/hqnqjkbe");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare("example-queue", exclusive: false);
+
+EventingBasicConsumer consumer = new(model: channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer);
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.ReadLine();
